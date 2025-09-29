7f2acc2294c9c20f5b3a7b9009a57a696e1d6756
Максим Столяр <stolyarmaks2207@gmail.com>
2025-09-29 17:26:54 +0300

    optimazed js code and added week forecast min and max temperature

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -4,17 +4,32 @@ const feelsLike=document.querySelector(".feels_like")
 const humidity=document.querySelector(".humidity")
 const wind=document.querySelector(".wind")
 const precipitation=document.querySelector(".precipitation")
-
-    fetch(`https://api.open-meteo.com/v1/forecast?latitude=50.4547&longitude=30.5238&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&hourly=temperature_2m,weather_code&current=temperature_2m,wind_speed_10m,relative_humidity_2m,weather_code,apparent_temperature&timezone=Europe%2FBerlin`).
-    then((response) => {
+const min_days_temp=document.querySelectorAll(".min_day")
+const max_days_temp=document.querySelectorAll(".max_day")
+const url=`https://api.open-meteo.com/v1/forecast?latitude=50.4547&longitude=30.5238&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&hourly=temperature_2m,weather_code&current=temperature_2m,wind_speed_10m,relative_humidity_2m,weather_code,apparent_temperature&timezone=Europe%2FBerlin`
+function getWeather(){
+     fetch(url)
+         .then((response) => {
         return response.json();
         })
-        .then((data) => {
-            console.log(data);
-            mainTemperature.innerText = `${Math.floor(data.current.temperature_2m)}°`;
-            feelsLike.innerText=`${Math.floor(data.current.apparent_temperature)}°`;
-            humidity.innerText=`${Math.floor(data.current.relative_humidity_2m)}%`
+         .then((data)=>{
+             console.log(data);
+             renderWeather(data);
+         })
+}
+
+// const weekDays=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+function renderWeather(data) {
+            mainTemperature.innerText = `${Math.round(data.current.temperature_2m)}°`;
+            feelsLike.innerText=`${Math.round(data.current.apparent_temperature)}°`;
+            humidity.innerText=`${Math.round(data.current.relative_humidity_2m)}%`
             wind.innerText=`${data.current.wind_speed_10m} km/h`
             precipitation.innerText=`${data.daily.precipitation_sum[0]} mm`
-        })
-
+    data.daily.temperature_2m_min.forEach((temp,i)=>{
+        min_days_temp[i].innerText=`${Math.round(temp)}°`
+    })
+    data.daily.temperature_2m_max.forEach((temp,i)=>{
+        max_days_temp[i].innerText=`${Math.round(temp)}°`
+    })
+}
+getWeather()
