332381f2941df3a24c09f979c960092c6527ee49
Максим Столяр <stolyarmaks2207@gmail.com>
2025-10-04 23:08:16 +0300

    add weather search for city

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,37 @@
 // спільні функції
+export const form = document.querySelector(".search_city");
+
+export async function getCity(cityName) {
+    const urlCity = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=en&format=json`;
+    try {
+        const memo = await fetch(urlCity);
+        const data = await memo.json();
+
+        console.log("Дані міста:", data);
+
+        if (data.results && data.results.length > 0) {
+            console.log("Координати:", data.results[0].latitude, data.results[0].longitude);
+            return data.results[0];
+        } else {
+            console.error("Місто не знайдено");
+            return null;
+        }
+    } catch (error) {
+        console.error("Помилка getCity: "+error);
+        return null;
+    }
+}
+    form.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        const city = form.querySelector("[name='input_city']");
+        const cityName = city.value.trim();
+        const cityData = await getCity(cityName);
+
+        if (cityData) {
+            const event = new CustomEvent('citySelected', {
+                detail: cityData
+            });
+            document.dispatchEvent(event);
+        } else {
+        }
+    });
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,35 +1,45 @@
 // логіка API, показ погоди
-const mainTemperature=document.querySelector(".content_info-temperature")
-const feelsLike=document.querySelector(".feels_like")
-const humidity=document.querySelector(".humidity")
-const wind=document.querySelector(".wind")
-const precipitation=document.querySelector(".precipitation")
-const min_days_temp=document.querySelectorAll(".min_day")
-const max_days_temp=document.querySelectorAll(".max_day")
-const url=`https://api.open-meteo.com/v1/forecast?latitude=50.4547&longitude=30.5238&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&hourly=temperature_2m,weather_code&current=temperature_2m,wind_speed_10m,relative_humidity_2m,weather_code,apparent_temperature&timezone=Europe%2FBerlin`
-function getWeather(){
-     fetch(url)
-         .then((response) => {
-        return response.json();
-        })
-         .then((data)=>{
-             console.log(data);
-             renderWeather(data);
-         })
+const mainTemperature = document.querySelector(".content_info-temperature");
+const feelsLike = document.querySelector(".feels_like");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const precipitation = document.querySelector(".precipitation");
+const min_days_temp = document.querySelectorAll(".min_day");
+const max_days_temp = document.querySelectorAll(".max_day");
+
+async function getWeather(latitude, longitude) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&hourly=temperature_2m,weather_code&current=temperature_2m,wind_speed_10m,relative_humidity_2m,weather_code,apparent_temperature&timezone=auto&timeformat=unixtime`;
+    try {
+        const meta = await fetch(url);
+        const data = await meta.json();
+        renderWeather(data);
+    } catch (error) {
+        console.error("Помилка getWeather: "+error);
+    }
 }
 
-// const weekDays=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
 function renderWeather(data) {
-            mainTemperature.innerText = `${Math.round(data.current.temperature_2m)}°`;
-            feelsLike.innerText=`${Math.round(data.current.apparent_temperature)}°`;
-            humidity.innerText=`${Math.round(data.current.relative_humidity_2m)}%`
-            wind.innerText=`${data.current.wind_speed_10m} km/h`
-            precipitation.innerText=`${data.daily.precipitation_sum[0]} mm`
-    data.daily.temperature_2m_min.forEach((temp,i)=>{
-        min_days_temp[i].innerText=`${Math.round(temp)}°`
-    })
-    data.daily.temperature_2m_max.forEach((temp,i)=>{
-        max_days_temp[i].innerText=`${Math.round(temp)}°`
-    })
+
+    mainTemperature.innerText = `${Math.round(data.current.temperature_2m)}°`;
+    feelsLike.innerText = `${Math.round(data.current.apparent_temperature)}°`;
+    humidity.innerText = `${Math.round(data.current.relative_humidity_2m)}%`;
+    wind.innerText = `${data.current.wind_speed_10m} km/h`;
+    precipitation.innerText = `${data.daily.precipitation_sum[0]} mm`;
+
+    data.daily.temperature_2m_min.forEach((temp, i) => {
+        if (min_days_temp[i]) {
+            min_days_temp[i].innerText = `${Math.round(temp)}°`;
+        }
+    });
+
+    data.daily.temperature_2m_max.forEach((temp, i) => {
+        if (max_days_temp[i]) {
+            max_days_temp[i].innerText = `${Math.round(temp)}°`;
+        }
+    });
 }
-getWeather()
+
+document.addEventListener('citySelected', (e) => {
+    const cityData = e.detail;
+    getWeather(cityData.latitude, cityData.longitude);
+});
